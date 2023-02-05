f0f4fd7411e2ce35af73e0da57e345923c373443
HUHARED <mailwork.hu@gmail.com>
2023-02-05 14:37:46 +0800

    优化鼠标移动计算。

--- 常用组件库V3_鼠标增强/.code/CSharp/傻逼云扩运算慢.cs
+++ 常用组件库V3_鼠标增强/.code/CSharp/傻逼云扩运算慢.cs
@@ -2,7 +2,7 @@
 public void Run()
 {
     //在这里编写您的代码
-    midpoint = totalDistance*3/5; // 加速度切换点
+    midpoint = totalDistance*4/5; // 加速度切换点
     // 当前位移小于预期总位移时，计算每次位移并加入列表。对应每秒10次位移
     while( currentDistance < totalDistance) { 
 		// 切换点前加速度+，切换点后加速度-
@@ -15,9 +15,9 @@ public void Run()
             //Console.WriteLine("slow down.");
         }
 
-        initialVelocity = velocity; // 当前循环初始速度=当前速度
+        velocity0 = velocity; // 当前循环初始速度=当前速度
 		
-        velocity = initialVelocity+currentAcceleration*time; // 计算本次位移速度
+        velocity = velocity0+currentAcceleration*time; // 计算本次位移速度
         
         xMove = velocity*time+1/2*currentAcceleration*time*time; // 计算本次位移距离
         currentDistance = currentDistance+xMove; // 计算当前总位移
@@ -26,6 +26,7 @@ public void Run()
             Console.WriteLine("currentDistance="+currentDistance);
         }
         if (xMove <= 0){ // 本次位移为0则多加几个0（模拟人的停顿，然后退出循环。
+            
             tracksList.Add(0);
             tracksList.Add(0);
             tracksList.Add(0);
@@ -34,6 +35,8 @@ public void Run()
             break;
         }
     }
+    Console.WriteLine("final currentDistance="+currentDistance);
+    Console.WriteLine("tracksList.Sum()="+tracksList.Sum());
 
 }
 //在这里编写您的函数或者类
