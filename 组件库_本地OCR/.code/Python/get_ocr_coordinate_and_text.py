5f315d07b02e571ee076971efef53275be39a867
Grit-Hu <mailwork.hu@gmail.com>
2023-08-19 17:49:07 +0800

    增加文字坐标识别_调用服务器.xaml

---
+++ 组件库_本地OCR/.code/Python/get_ocr_coordinate_and_text.py
@@ -0,0 +1,22 @@
+﻿#-*- coding:utf-8 -*-
+import sys
+import json
+import requests
+
+if __name__ == "__main__":
+    inStrImagePath=sys.argv[1]
+
+r = requests.post("http://127.0.0.1:5000/ocr", headers={
+    "Content-Type": "application/json"},data=json.dumps({"imgPath": inStrImagePath}))
+# print(r)
+tempArray = []
+for arrayElement in json.loads(r.text)[0]:
+    print(arrayElement)
+    arrayTopLeft=[str(int(arrayElement[0][0][0])),str(int(arrayElement[0][0][1]))]
+    print("arrayTopLeft="+str(arrayTopLeft))
+    strText=arrayElement[1][0]
+    print("strText="+strText)
+    tempArray.append({"coordinate":arrayTopLeft,"text":[strText]})
+print("----Recognized result array----")
+print(tempArray)
+outStrOCRResultJSON=json.dumps(tempArray,ensure_ascii=True)
---
+++ 组件库_本地OCR/.code/Python/独立识别 - 副本.py
@@ -0,0 +1,11 @@
+﻿#-*- coding:utf-8 -*-
+import sys
+import PaddleOCR.ocr_paddle2
+
+
+if __name__ == "__main__":
+    imagePath=sys.argv[1]
+    language=sys.argv[2]
+    resultText=""
+
+resultText=PaddleOCR.ocr_paddle2.recognize(imagePath,language)
